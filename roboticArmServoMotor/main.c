aaf45aec812db08fbbabdb401c156f58c3c3842c
ramankooner <ramankooner9@gmail.com>
2019-07-28 13:45:28 -0700

    Added PWM numbers for angles

--- roboticArmServoMotor/main.c
+++ roboticArmServoMotor/main.c
@@ -11,8 +11,9 @@ int main(void){
 
 	PortF_Init(); //On-board LEDs
 	GPIO_PORTF_DATA_R = 0x02;
-	M0PWM3_Init(40000, 10000); //PB5
-	
+	//M0PWM3_Init(40000, 35000); //PB5   SECOND JOINT
+	M0PWM3_Init(40000, 35500); // PB5 HAND - 35500 CLOSE/30000 OPEN
+	//M1PWM3_Init(40000, 5000); //PA7    THIRD JOINT
 	while(1){
 		GPIO_PORTF_DATA_R = 0x08;
 	}
